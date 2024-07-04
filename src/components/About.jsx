bfe1f9cd7fc19de9742fc8db2d0cc2032d63a4c9
Anmol Singh <as0796348@gmail.com>
2024-07-04 10:48:32 +0530

    About section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import AboutImg from "../assets/About.jpg"
+import { ABOUT } from "../constants"
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+    return (
+        <div>
+            <div className="flex justify-center items-center gap-20">
+                <img src={AboutImg} alt={AboutImg} className="h-[26rem]" />
+                <div className="w-[35rem] mt-20">
+                    <h1 className="text-4xl text-neutral-800 font-semibold mb-8">What your BMI result means</h1>
+                    {ABOUT.map((about, index) => (
+                        <div key={index}>
+                            <p className="font-semibold text-neutral-400">{about.aboutBMI}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default About
