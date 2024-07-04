bf4acde1398118519b06437c9da4f577cf2665ef
Anmol Singh <as0796348@gmail.com>
2024-07-04 14:16:51 +0530

    changes added

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,4 +8,10 @@ export const ABOUT = [
     {
         aboutBMI: "Your Body Mass Index (BMI) is a measure of body fat based on height and weight. It categorizes you as underweight (BMI < 18.5), normal weight (BMI 18.5-24.9), overweight (BMI 25-29.9), or obese (BMI ≥ 30). Underweight may indicate nutritional deficiencies; normal weight suggests a healthy weight; overweight increases risk of health issues; and obesity significantly raises risk of severe health conditions. While useful, BMI doesn't differentiate between muscle and fat, so consider it alongside other health factors like diet and activity level."
     }
+];
+
+export const LIMITATIONS = [
+    {
+        disAdvantages: "BMI, while widely used, has limitations including its inability to distinguish between muscle mass and fat content, potentially misclassifying athletes and individuals with high muscle mass as overweight or obese. It also does not account for the distribution of fat throughout the body, which is a significant factor in assessing health risks."
+    }
 ]
