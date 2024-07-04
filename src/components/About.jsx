6fd676b6f07c1f1f4e5e45b640b4b644f675104e
Anmol Singh <as0796348@gmail.com>
2024-07-04 10:49:13 +0530

    About section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,17 +3,15 @@ import { ABOUT } from "../constants"
 
 const About = () => {
     return (
-        <div>
-            <div className="flex justify-center items-center gap-20">
-                <img src={AboutImg} alt={AboutImg} className="h-[26rem]" />
-                <div className="w-[35rem] mt-20">
-                    <h1 className="text-4xl text-neutral-800 font-semibold mb-8">What your BMI result means</h1>
-                    {ABOUT.map((about, index) => (
-                        <div key={index}>
-                            <p className="font-semibold text-neutral-400">{about.aboutBMI}</p>
-                        </div>
-                    ))}
-                </div>
+        <div className="flex justify-center items-center gap-20">
+            <img src={AboutImg} alt={AboutImg} className="h-[26rem]" />
+            <div className="w-[35rem] mt-20">
+                <h1 className="text-4xl text-neutral-800 font-semibold mb-8">What your BMI result means</h1>
+                {ABOUT.map((about, index) => (
+                    <div key={index}>
+                        <p className="font-semibold text-neutral-400">{about.aboutBMI}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
