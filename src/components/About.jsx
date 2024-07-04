191c5f4e2e01cac5f50d9ab22109073a937cc4a8
Anmol Singh <as0796348@gmail.com>
2024-07-04 11:44:03 +0530

    About section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className="flex justify-center items-center gap-20">
             <img src={AboutImg} alt={AboutImg} className="h-[26rem]" />
             <div className="w-[35rem] mt-20">
-                <h1 className="text-4xl text-neutral-800 font-semibold mb-8">What your BMI result means</h1>
+                <h1 className="text-4xl text-neutral-800 font-semibold mb-8">What your BMI result means ?</h1>
                 {ABOUT.map((about, index) => (
                     <div key={index}>
                         <p className="font-semibold text-neutral-400">{about.aboutBMI}</p>
