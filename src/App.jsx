70e012723fb40b4a2ec020ae4a7942cb86152f06
Anmol Singh <as0796348@gmail.com>
2026-06-18 22:53:13 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
           <div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
         </div>
         <div className="container mx-auto px-8">
-          <Hero />z
+          <Hero />
           <About />
           <Features />
           <Limitations />
